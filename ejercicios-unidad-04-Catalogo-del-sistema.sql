0c481e2435ff5be0bc5b23057e84c8deb8e2a9d1
Facundo Berges <facundo.h.berges@gmail.com>
2024-09-29 22:55:48 -0300

    ejercicios de consultas a catalogo del sistema

---
+++ ejercicios-unidad-04-Catalogo-del-sistema.sql
@@ -0,0 +1,23 @@
+--  Consultas a catálogo.
+
+-- 1) Cuáles tablas contienen la columna LOCALIDAD?
+
+SELECT DISTINCT C.TABLE_NAME
+FROM INFORMATION_SCHEMA.COLUMNS C
+WHERE UPPER(C.COLUMN_NAME) = 'LOCALIDAD';
+
+-- 2) Cuántas columnas tiene la tabla PRODUCTOS?
+
+SELECT COUNT(*) AS CANTIDAD_COLUMNAS_EN_TABLA_PRODUCTOS
+FROM INFORMATION_SCHEMA.COLUMNS C
+WHERE UPPER(C.TABLE_NAME) = 'PRODUCTOS';
+
+-- 3) Obtener una lista de todos los usuarios dueños de por lo menos una tabla, junto con el número de tablas que le pertenecen a cada uno.
+
+
+
+-- 4) Obtener una lista de los nombres de todas las tablas que tienen por lo menos un índice.
+
+SELECT DISTINCT TABLE_NAME
+FROM INFORMATION_SCHEMA.STATISTICS
+WHERE UPPER(TABLE_SCHEMA) = 'COURSE-DB';
